db8c3e25cbb64e372e22e1860643dc514f7fb36f
Matthew Stewart <stewartmatthew6@gmail.com>
2024-04-10 15:01:54 -0400

    Installed mdx package

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import mdx from '@mdx-js/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     viteTsconfigPaths(),
+    mdx(),
   ],
   server: {
     port: 3000,
