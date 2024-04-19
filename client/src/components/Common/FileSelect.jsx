df0dc44725c9ff9d851d2357d8037862f7f3d9e2
Matthew Stewart <stewartmatthew6@gmail.com>
2024-04-19 13:21:19 -0400

    Refactored FileSelect.jsx

--- client/src/components/Common/FileSelect.jsx
+++ client/src/components/Common/FileSelect.jsx
@@ -1,66 +1,57 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+
 import {checkSum} from '../Utility/Storage';
 
-class FileSelect extends Component{
-  constructor(){
-    super();
-    this.state = {
-      failing: false,
-    }
-    this.fileInput = React.createRef();
-    this.game = 'Seasons';
-    this.handleFile = this.handleFile.bind(this);
-    this.inputDisplay = this.inputDisplay.bind(this);
-  }
+function FileSelect(props) {
+  const [failing, setFailing] = useState(false);
 
-  handleFile(){
-    const passed = checkSum(this.fileInput.current.files[0], this.props.game, this.props.checkGame);
+  const fileInput = React.createRef();
+  var game = 'Seasons';
+
+  function handleFile(){
+    const passed = checkSum(fileInput.current.files[0], props.game, props.checkGame);
     if (!passed) {
-      this.setState({
-        failing: true
-      })
+      setFailing(true);
     }
   }
 
-  inputDisplay(text, cName) {
-    if (this.props.valid) {
+  function inputDisplay(text, cName) {
+    if (props.valid) {
       const validClass = ['border', 'border-success', 'alert-success', 'h-100', 'pt-1', 'text-center'];
       return(
-        <div className={validClass.join(' ')}>Oracle of {this.props.game} Rom Loaded</div>
+        <div className={validClass.join(' ')}>Oracle of {props.game} Rom Loaded</div>
       )
     } else {
       const inputClass = ['custom-file'];
-      if (this.props.classes){
-        this.props.classes.forEach(classname=>{inputClass.push(classname)})
+      if (props.classes){
+        props.classes.forEach(classname=>{inputClass.push(classname)})
       }
       return (
         <div className="custom-file">
-          <input type="file" name="file" id="file" className="custom-file-input" ref={this.fileInput} onChange={this.handleFile} />
+          <input type="file" name="file" id="file" className="custom-file-input" ref={fileInput} onChange={handleFile} />
           <label htmlFor="file" className={cName.join(' ')}>{text}</label>
         </div>
       )
     }
   }
 
-  render() {
-    // Should only display error message on hash not matching. Should disappear on game change.
-    const shouldDisplayError = this.state.failing && this.game === this.props.game;
-    const text = shouldDisplayError ? `Not a valid Oracle of ${this.props.game} rom`: `Select Oracle of ${this.props.game} Rom (English)`;
-    const cName = ['custom-file-label'];
+  // Should only display error message on hash not matching. Should disappear on game change.
+  const shouldDisplayError = failing && game === props.game;
+  const text = shouldDisplayError ? `Not a valid Oracle of ${props.game} rom`: `Select Oracle of ${props.game} Rom (English)`;
+  const cName = ['custom-file-label'];
 
-    if (shouldDisplayError){
-      cName.push('bg-danger', 'text-light')
-    }
-
-    const input = this.inputDisplay(text, cName);
-
-    this.game = this.props.game;
-    return (
-      <div className="col-md">
-        {input}
-      </div>
-    )
+  if (shouldDisplayError){
+    cName.push('bg-danger', 'text-light')
   }
+
+  const input = inputDisplay(text, cName);
+
+  game = props.game;
+  return (
+    <div className="col-md">
+      {input}
+    </div>
+  )
 }
 
 export default FileSelect;
