7f64d0c01553d09b9f69fe7a811a33d070aeeb7f
Matthew Stewart <stewartmatthew6@gmail.com>
2024-05-19 19:08:01 -0400

    Removed dead code

--- client/src/components/Utility/Patcher.js
+++ client/src/components/Utility/Patcher.js
@@ -3,65 +3,6 @@ import {readPointer,writePointer} from './RomHelper';
 
 const agesLinkObjectPaletteOffsets = [82446,82448,82450,82452,82454,82456,82458,82460,82462,82464];
 
-function patchPaletteData(rom_array, paletteIndex, ages){
-  const LinkPaletteOffsets = ages ? agesLinkObjectPaletteOffsets : agesLinkObjectPaletteOffsets.map((x,i)=>{
-    return x - 66;
-  });
-
-  LinkPaletteOffsets.forEach(offset=>{
-    rom_array[offset] = rom_array[offset] | paletteIndex;
-  });
-
-  const linkOamTable = ages ? 0x1a0a7 : 0x19d9e;
-  const oamBank = ages ? 0x13 : 0x12;
-
-  // Preserve the palettes of everything that's not link's color (ie. harp)
-  for (let i=0; i<48; i++) {
-    let addr = readPointer(rom_array, linkOamTable + i*2, oamBank);
-    let count = rom_array[addr];
-    addr++;
-    for (let j=0; j<count; j++) {
-      if ((rom_array[addr+3] & 7) !== 0) {
-        if (i >= 46 && paletteIndex  >= 4) // Harp only: use inverted palettes
-          rom_array[addr+3] = (rom_array[addr+3] & ~7);
-        else // Everything else: use its original palette
-          rom_array[addr+3] ^= paletteIndex;
-      }
-      addr += 4;
-    }
-  }
-
-  // Address of standard sprite palettes
-  const paletteSrc = ages ? 0x5c8f0 : 0x58840;
-  // Free space to put modified copy of sprite palettes for file select screen
-  const fileSelectPaletteAddr = ages ? 0x5eee3 : 0x5ba07;
-  // Copy normal palettes
-  for (let x=8; x<8*4; x++)
-    rom_array[fileSelectPaletteAddr + x] = rom_array[paletteSrc + x];
-  // Replace first palette (green) with appropriate one
-  for (let x=0; x<8; x++)
-    rom_array[fileSelectPaletteAddr + x] = rom_array[paletteSrc + x + paletteIndex * 8];
-  // Replace file select's palette reference to newly created data
-  if (ages) {
-    writePointer(rom_array, 0x64de, fileSelectPaletteAddr);
-    writePointer(rom_array, 0x64ea, fileSelectPaletteAddr);
-  }
-  else {
-    writePointer(rom_array, 0x6428, fileSelectPaletteAddr);
-    writePointer(rom_array, 0x6434, fileSelectPaletteAddr);
-  }
-
-  if (paletteIndex >= 4) {
-    // For "inverted" palettes, change the damage-taking palette to 1 or
-    // 2 (depending if it's red or blue).
-    const damagePalette = (paletteIndex-3) | 8;
-    if (ages)
-      rom_array[0x14290] = damagePalette;
-    else
-      rom_array[0x1424e] = damagePalette;
-  }
-}
-
 function finalize(rom_array, romName){
   // All patches applied. Recalculate rom checksum.
   var checksum = 0
